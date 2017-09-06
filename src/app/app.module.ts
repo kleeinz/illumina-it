c5eb34cca5182ce6e44e93f67f3a21b98629bd8a
Christian Lopez Trejo <1412596@SOAM.TCS.com>
2017-09-06 15:59:57 -0500

    Adding children components to home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { APP_ROUTING } from './app.routes';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
 import { NavBarComponent } from './components/navbar/navbar.component';
+import { ClientsComponent } from './components/clients/clients.component';
 
 @NgModule({
   declarations: [
   	LoginComponent,
   	HomeComponent,
     NavBarComponent,
+    UsersComponent,
+    ClientsComponent,
     AppComponent
   ],
   imports: [
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { UsersComponent } from './components/users/users.component';
+import { ClientsComponent } from './components/clients/clients.component';
 
 const APP_ROUTES: Routes = [
-	{ path: 'home', component: HomeComponent },
-	{ path: 'login', component: LoginComponent },
-	{ path: '**', pathMatch: 'full', redirectTo: 'login' }
+{ path: 'home', component: HomeComponent , 
+children:[
+{path: 'users', component: UsersComponent},
+{path: 'clients', component: ClientsComponent}
+]
+},
+{ path: 'login', component: LoginComponent },
+{ path: '**', pathMatch: 'full', redirectTo: 'login' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
