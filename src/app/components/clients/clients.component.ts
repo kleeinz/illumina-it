42192be1c1334dc78d897eb938a937e9bca101eb
Christian Lopez Trejo <1412596@SOAM.TCS.com>
2017-09-06 16:00:16 -0500

    Adding children components to home component

---
+++ src/app/components/clients/clients.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'clients',
+	templateUrl: 'clients.component.html',
+})
+export class ClientsComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {
+		
+	}
+}
