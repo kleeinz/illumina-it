e5403bb39597493fc11a833709ad73fdf0714610
Christian Omar Lopez Trejo <kleeinzz@gmail.com>
2017-09-19 23:14:40 -0500

    Making any changes in the Filter Pipe

--- src/app/components/shared/pipes/filter-datatable.pipe.ts
+++ src/app/components/shared/pipes/filter-datatable.pipe.ts
@@ -6,9 +6,11 @@ import {Pipe, PipeTransform} from "@angular/core";
 })
 export class FilterDataTablePipe implements PipeTransform {
 
-  transform(values: any[], input: string) : any {
-     if (input)
+  transform(values: any[], input: string, model: string) : any {
+     if (input && model === "users")
         return _.filter(values, row => row.username.indexOf(input) > -1);
+     if (input && model === "clients")
+        return _.filter(values, row => row.name.indexOf(input) > -1);
       return values;
   }
 
